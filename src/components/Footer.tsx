5edf771e7ec63a5b6096c0f1ccacb3430194623b
Dario George <edu.dariogeorge21@gmail.com>
2025-07-20 19:17:09 +0530

    buymeacoffee
    Signed-off-by: Dario George <edu.dariogeorge21@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -204,7 +204,7 @@ export default function Footer() {
         {/* Bottom section */}
         <div className="pt-6 sm:pt-8 border-t border-gray-800/50 flex flex-col sm:flex-row justify-between items-center">
           <div className="text-gray-400 text-xs sm:text-sm tech-text text-center sm:text-left">
-            &copy; {currentYear} AI Central Station by <span className="text-blue-400">Dario George</span>. All rights reserved. 
+            &copy; {currentYear} AI Central Station by <span className="text-blue-400">Dario George</span>. All rights reserved.  
             <a
               href="https://buymeacoffee.com/dariogeorge21"
               target="_blank"
